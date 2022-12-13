4abdd5d112b5f9e76c65b064c6f0ca84ff0bbc68
briancastro-bc <brian.castro.bedoya@gmail.com>
2022-12-13 09:24:22 -0500

    Update some features

--- src/app/shared/components/layout/navigation/navigation.component.ts
+++ src/app/shared/components/layout/navigation/navigation.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+export interface Links {
+  route: string;
+  label: string;
+}
+
 @Component({
   selector: 'yt-navigation',
   template: `
     <div class="navigation">
       <nav class="navigation__links">
-        <h1>Navigation works</h1>
+        <a class="navigation__links--link" *ngFor="let link of navigationLinks">
+          |
+          <span class="label">{{ link.label | translate }}</span>
+        </a>
       </nav>
     </div>
   `,
@@ -14,10 +22,31 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class NavigationComponent implements OnInit {
 
+  readonly navigationLinks: Links[];
+
   constructor(
     private readonly router: Router,
     private readonly activatedRoute: ActivatedRoute,
-  ) {}
+  ) {
+    this.navigationLinks = [
+      {
+        route: '/',
+        label: 'app.navigation.home.label',
+      },
+      {
+        route: '/about',
+        label: 'app.navigation.about.label',
+      },
+      {
+        route: '/works',
+        label: 'app.navigation.works.label',
+      },
+      {
+        route: '/contact',
+        label: 'app.navigation.contact.label',
+      }
+    ]
+  }
 
   ngOnInit(): void {}
 }
